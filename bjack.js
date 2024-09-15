cf0cb995783a78d6f78ae9c8a08d99686f08dc20
Connor Swalm <connorswalm@gmail.com>
2024-09-14 20:11:41 -0400

    changed game structure to support multiple hands in the future

--- bjack.js
+++ bjack.js
@@ -2,7 +2,7 @@ const prompt = require("prompt-sync")({ sigint: true });
 const { makeShoe } = require("./shoe");
 const { calculateCorrectMove } = require("./checkCorrectMove");
 const { testHandKnowledge } = require("./testHandKnowledge");
-const { deal } = require("./game");
+const { deal, blackJack } = require("./game");
 
 function main() {
   console.log("What game would you like to play?");
@@ -19,79 +19,10 @@ function main() {
   }
 
   while (gameChoice === "p") {
-    let game = {
-      players: [],
-      shoe: [],
-      dealer: [],
-    };
-    //play blackjack
-
-    // make a shoe
-    // set how many players to begin dealing to
-    // always ensure you have enough cards
-    // deal a hand
-    // cleanup
-
-    let numberOfDecks = prompt("How many decks are we playing with: ");
-
-    const shoe = makeShoe(parseInt(numberOfDecks));
-
-    // console.log(shoe);
-    let numberOfPlayers = prompt("How many hands are we playing with: ");
-
-    let players = [];
-    for (let i = 0; i < numberOfPlayers; i++) {
-      players.push([]);
+    const userFeedback = blackJack();
+    if (userFeedback === "n") {
+      break;
     }
-    game.players = players;
-    game.shoe = shoe;
-    console.log(game);
-
-    let continuePlaying = true;
-    console.log("Begin Play!");
-    while (continuePlaying) {
-      // place bets eventually
-      let { players, shoe, dealer } = game;
-      players.forEach((player) => player.push(shoe.shift()));
-      dealer.push(shoe.shift());
-
-      players.forEach((player) => player.push(shoe.shift()));
-      dealer.push(shoe.shift());
-
-      console.log(`Dealer is showing a ${dealer[0]}`);
-      players.forEach((player, i) => console.log(`Player ${i} has ${player}`));
-      for (let i = 0; i < players.length; i++) {
-        console.log(`Player ${i}, you have ${players[i]}`);
-        let playerChoice = prompt(
-          `Dealer is showing ${dealer[0]}, what would you like to do: `
-        );
-        let currentPlayersMove = true;
-        let currentPlayer = [];
-        while (currentPlayersMove) {
-          if (playerChoice === "n") {
-            continuePlaying = false;
-            gameChoice = "m";
-            currentPlayersMove = false;
-            break;
-          } else if (playerChoice === "s") {
-            console.log("stand");
-            currentPlayersMove = false;
-          } else if (playerChoice === "d") {
-            currentPlayer.push(shoe.shift());
-          } else if (playerChoice === "sp") {
-            // add support to add a player
-          } else if (playerChoice === "h") {
-            currentPlayer.push(shoe.shift);
-          }
-        }
-      }
-    }
-    // start playing
-
-    // deal a card to every player and the dealer
-    // deal a 2nd card to every player
-
-    // let a player play their entire hand until they stand or bust
   }
 }
 
--- game.js
+++ game.js
@@ -1,3 +1,7 @@
+const { makeShoe } = require("./shoe");
+const prompt = require("prompt-sync")({ sigint: true });
+const { tens } = require("./consts");
+
 function deal(shoe) {
   if (shoe.length < 12) {
     throw new Error("Shoe must contain at least 12 elements.");
@@ -32,4 +36,139 @@ function deal(shoe) {
   };
 }
 
+const initializeGame = (numberOfDecks, numberOfPlayers) => {
+  const shoe = makeShoe(parseInt(numberOfDecks));
+  let players = {};
+  for (let i = 0; i < numberOfPlayers; i++) {
+    players[`Player${i}`] = {
+      hand1: [],
+    };
+  }
+  return {
+    shoe,
+    players,
+    dealer: [],
+  };
+};
+
+const dealRoundOfCards = (game) => {
+  let { players, shoe, dealer } = game;
+
+  const numberOfPlayers = Object.keys(players).length;
+
+  for (let i = 0; i < 2; i++) {
+    for (let j = 0; j < numberOfPlayers; j++) {
+      let playerHand = players[`Player${j}`].hand1;
+      playerHand.push(shoe.shift());
+      dealer.push(shoe.shift());
+      players[`Player${j}`].hand1 = playerHand;
+    }
+  }
+  return { players, dealer, shoe };
+};
+
+const calculateHandTotal = (playerHand) => {
+  let handBusts = false;
+  let handTotal = 0;
+  if (playerHand.includes("A")) {
+    // figure out soft totals
+  } else {
+    playerHand.forEach((card) => {
+      if (tens.includes(card)) {
+        handTotal += 10;
+      } else {
+        handTotal += parseInt(card);
+      }
+    });
+  }
+
+  return handTotal;
+};
+
+function blackJack() {
+  /*
+    game: {
+        dealer: [],
+        players: [[]],
+        shoe: []
+
+    }
+
+    need to shift to :
+    game: {
+        dealer: []
+        players: {
+            player: {
+                hand1: [],
+                hand2: []
+            }
+        shoe: []
+    }
+
+    */
+
+  let numberOfDecks = prompt("How many decks are we playing with: ");
+  let numberOfPlayers = prompt("How many hands are we playing with: ");
+
+  let game = initializeGame(numberOfDecks, numberOfPlayers);
+  let continuePlaying = true;
+  console.log(game);
+  while (continuePlaying) {
+    // place bets eventually
+
+    let { players, dealer, shoe } = dealRoundOfCards(game);
+
+    console.log(`Dealer is showing ${dealer[0]}`);
+    console.log(players);
+    console.log("------------------");
+    // now we play
+
+    let numberOfPlayers = Object.keys(players).length;
+    for (let i = 0; i < numberOfPlayers; i++) {
+      let currentPlayer = players[`Player${i}`];
+      let currentHandIndex = 0;
+      // {hand1: []}
+
+      let stillPlayersTurn = true;
+      while (stillPlayersTurn) {
+        console.log(`Player ${i}, you have ${players[i]}`);
+        let playerChoice = prompt(
+          `Dealer is showing ${dealer[0]}, what would you like to do: `
+        );
+
+        if (playerChoice === "n") {
+          stillPlayersTurn = false;
+          return playerChoice;
+        } else if (playerChoice === "s") {
+          // move to next player
+          stillPlayersTurn = false;
+        } else if (playerChoice === "h") {
+          const dealtCard = shoe.shift();
+          console.log(`You are dealt a ${dealtCard}`);
+
+          currentPlayer["hand1"] = [...currentPlayer.hand1, dealtCard];
+          console.log(`Hand ${currentPlayer["hand1"]}`);
+          const handTotal = calculateHandTotal(currentPlayer["hand1"]);
+          if (handTotal > 21) {
+            console.log(` Player ${i} busts!`);
+            stillPlayersTurn = false;
+          }
+        } else if (playerChoice === "d") {
+          // work in bets in the future
+          const dealtCard = shoe.shift();
+          console.log(`You are dealt a ${dealtCard}`);
+          currentPlayer.push(dealtCard);
+          stillPlayersTurn = false;
+        } else if (playerChoice === "sp") {
+          // add temporary player
+        }
+      }
+    }
+
+    continuePlaying = false;
+    return "n";
+  }
+}
+
+module.exports.blackJack = blackJack;
 module.exports.deal = deal;
